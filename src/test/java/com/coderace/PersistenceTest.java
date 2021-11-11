debfe893f6d4987b49be49ddc6727838a948db14
Ortola <nicolas.ortola@mercadolibre.com>
2021-11-11 13:42:27 -0300

    fix test

--- src/test/java/com/coderace/PersistenceTest.java
+++ src/test/java/com/coderace/PersistenceTest.java
@@ -1,12 +1,11 @@
 package com.coderace;
 
 import com.coderace.entity.Person;
+import com.coderace.entity.enums.Country;
 import com.coderace.repository.PersonRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,17 +17,17 @@ public class PersistenceTest {
 
     @Test
     void persistenceTest() {
-/*        final Person person = new Person("Nei", 29);
+        final Person person = new Person("Nei", 29, Country.ARG);
 
         repository.save(person);
 
-        final Optional<Person> persistedEntity = repository.findById(1);
+        final Person persistedEntity = repository.findById(1).orElseThrow(RuntimeException::new);
 
         assertAll("Expected entity",
-                () -> assertTrue(persistedEntity.isPresent()),
-                () -> assertEquals("Nei", persistedEntity.get().getName()),
-                () -> assertEquals(29, persistedEntity.get().getAge())
-        );*/
+                () -> assertEquals("Nei", persistedEntity.getName()),
+                () -> assertEquals(29, persistedEntity.getAge()),
+                () -> assertEquals(Country.ARG, persistedEntity.getCountry())
+        );
     }
 
     @Test
